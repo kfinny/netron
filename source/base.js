fbeb6997679061a54750af4815f7653ae98eafb7
Kevin Finnigin <kevin@finnigin.net>
2024-12-20 11:14:41 -0500

    Update base.js

--- source/base.js
+++ source/base.js
@@ -1210,6 +1210,7 @@ base.Telemetry = class {
     }
 
     send(name, params) {
+        return;
         if (this._session) {
             try {
                 params = { event_name: name, ...this._metadata, ...params };
@@ -1222,7 +1223,7 @@ base.Telemetry = class {
                 this._cache = this._cache || build(Array.from(this._config));
                 const key = (name, value) => this._schema.get(name) || (typeof value === 'number' && !isNaN(value) ? 'epn.' : 'ep.') + name;
                 const body = build(Object.entries(params).map(([name, value]) => [key(name, value), value]));
-                const url = `https://analytics.google.com/g/collect?${this._cache}`;
+                // const url = `https://analytics.google.com/g/collect?${this._cache}`;
                 this._navigator.sendBeacon(url, body);
                 this._session[2] = this.get('session_engaged') || '0';
                 this.set('hit_count', this.get('hit_count') + 1);
